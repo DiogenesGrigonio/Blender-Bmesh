9dbbb1868da348c8bed8e8770de8027ad2a3c2a4
DiogenesGrigonio <modofino@hotmail.com>
2024-10-20 12:27:26 -0300

    Update bmesh_ops.py

--- bmesh_ops.py
+++ bmesh_ops.py
@@ -24,6 +24,43 @@ def create_face(BMesh, Edge, Vector):
     return E1
 
 
+def unselect_all(BMesh):
+    BMesh.select_mode = {'VERT', 'EDGE', 'FACE'}
+    for v in BMesh.verts:v.select_set(False)
+    BMesh.select_flush_mode()
+
+
+def select_el(BMesh, List):
+    for el in List:el.select_set(True)
+    BMesh.select_flush(True)
+
+
+def convert_list(BMesh, InGroup, TypeIn, TypeOut):
+    unselect_all(BMesh)
+    if TypeIn=='Vert' and TypeOut=='Edge':
+        select_el(BMesh, InGroup)
+        OutGroup = [e for e in BMesh.edges if e.select] 
+    elif TypeIn=='Vert' and TypeOut=='Face':
+        select_el(BMesh, InGroup)
+        OutGroup = [f for f in BMesh.faces if f.select]
+    elif TypeIn=='Edge' and TypeOut=='Vert':
+        OutGroup = []
+        for item in [v for e in BMesh.edges 
+                    if e in InGroup 
+                    for v in e.verts]:
+            if item not in OutGroup:OutGroup.append(item)
+    elif TypeIn=='Edge' and TypeOut=='Face':
+        select_el(BMesh, InGroup)
+        OutGroup = [f for f in BMesh.faces if f.select]
+    elif TypeIn=='Face' and TypeOut=='Vert':
+        OutGroup = list({v for f in InGroup for v in f.verts})
+    elif TypeIn=='Face' and TypeOut=='Edge':
+        select_el(BMesh, InGroup)
+        OutGroup = [e for e in BMesh.edges if e.select]
+    unselect_all(BMesh)
+    return OutGroup
+
+
 def curtains(BMesh, FacesIn, CurtainType, CurtainResol, Seed):
     FaceToCurtains = [f for f in FacesIn if len(f.verts)==4]
     Thick = 0.03
@@ -33,9 +70,9 @@ def curtains(BMesh, FacesIn, CurtainType, CurtainResol, Seed):
     for Face in FaceToCurtains:
         NormalX = Face.normal[0]
         NormalY = Face.normal[1]
-        VerGroup = [vert.co for vert in Face.verts]
-        MinZ = min([vert[2] for vert in VerGroup])
-        SizeZ = max([vert[2] for vert in VerGroup]) - MinZ
+        VertGroup = [vert.co for vert in Face.verts]
+        MinZ = min([vert[2] for vert in VertGroup])
+        SizeZ = max([vert[2] for vert in VertGroup]) - MinZ
         VertLow = [v for v in Face.verts if v.co[2]==MinZ]
 
         if len(VertLow)!=2:
@@ -51,21 +88,19 @@ def curtains(BMesh, FacesIn, CurtainType, CurtainResol, Seed):
         else:
             VertY = max([VertLow[0].co[1], VertLow[1].co[1]])
 
+        #"""Curtain A"""
         if CurtainType==1:
-            """Curtain A"""
-            #variables
-            random.seed(Seed+Face.index)
-            LocalZ =  -SizeZ/CurtainResol*random.random()
+            LocalZ =  -SizeZ/CurtainResol
             
             #coordenadas para Vert 0 - 'Vert0Co'
             VertZ = VertLow[0].co[2]+SizeZ
             Vert0Co = [VertX-NormalX*0.02, VertY-NormalY*0.02, VertZ]
 
-            #create Vert 0
+            #Vert 0
             v = bmesh.ops.create_vert(BMesh,co=Vert0Co)
             V0 = v['vert'][0]
             
-            #create Edge 0
+            #Edge 0
             ret = bmesh.ops.extrude_vert_indiv(
                         BMesh,
                         verts=[V0])
@@ -77,8 +112,10 @@ def curtains(BMesh, FacesIn, CurtainType, CurtainResol, Seed):
             EdgeCurtain.extend(E1)
             del v, ret, E1
 
-            #create faces
-            for i in range(int(CurtainResol/3)):
+            #Faces
+            random.seed(Seed+Face.index)
+            RangeCurtain = 1+int(SizeZ*CurtainResol*random.uniform(-0.2,1.1)/4)
+            for i in range(RangeCurtain):
                 #face1
                 E1 = create_face(BMesh, 
                                 EdgeCurtain[-1], 
@@ -100,16 +137,13 @@ def curtains(BMesh, FacesIn, CurtainType, CurtainResol, Seed):
                                 (NormalX*Thick,NormalY*Thick,0))
                 EdgeCurtain.extend(E1)
 
+        #"""Curtain B"""
         elif CurtainType==2:
-            """Curtain B"""
-            #variables
-            CurtLow=0.5/CurtainResol
-            CurtHigh=0.4/CurtainResol
+            EdgeLocal = []
                             
             #coordenadas para Vert 0 - 'Vert0Co'
             VertZ = VertLow[0].co[2]-0.01
             Vert0Co = [VertX, VertY, VertZ]
-            EdgeLocal = []
 
             #Folha L
             EdgeLocal = []
@@ -177,23 +211,11 @@ def curtains(BMesh, FacesIn, CurtainType, CurtainResol, Seed):
                     v.co = (v.co[0]+NormalX*random.uniform(0.105,0),
                             v.co[1]+NormalY*random.uniform(0.105,0),
                             v.co[2])
+            del EdgeLocal
 
-            for e in EdgeLocal:e.select = True
-            BMesh.select_flush(True)
-            FaceLocal = [f for f in BMesh.faces if f.select]
-            for face in FaceLocal:face.normal_flip()
-            del EdgeLocal, FaceLocal
-            for face in BMesh.faces:face.select_set(False)
-            BMesh.select_flush(False)
-
-                                            
-    for e in EdgeCurtain:e.select = True
-    BMesh.select_flush(True)
-    FaceCurtain = [f for f in BMesh.faces if f.select]
-
+    FaceCurtain = convert_list(BMesh, EdgeCurtain, 'Edge', 'Face')
     if CurtainType==2:
         for face in FaceCurtain:face.smooth = True
-        
-    for face in FaceCurtain:face.normal_flip() 
+
 
     return FaceCurtain
